860c6023cd6c8b26fc582ebf369fcde4b4596616
Andy <code@andywilson.me.uk>
2019-11-20 09:32:06 +0000

    Implemented submit on Enter

--- src/components/view.js
+++ src/components/view.js
@@ -10,23 +10,33 @@ const focusAnswerBox = () =>
         if (answerBox === null) { return; }
         answerBox.focus();
     }, 50);
+const submitOnEnter = (state, evt) => (evt.key === 'Enter' ? actions.submit : state);
+
+const StartButton = () => <button onclick={[actions.prepare, focusAnswerBox]} class="start">Start</button>;
+const Question = ({ state }) =>
+    <div>
+        <h2>Question {state.questionIndex + 1}</h2>
+        <input id="answerBox" type="number" min="0" max="99" minlength="1" maxlength="2"
+            onchange={actions.setAnswerText} value={state.answerText} onkeyup={submitOnEnter}
+            spellcheck="false" autocomplete="off" />
+        <button onclick={[actions.submit, focusAnswerBox]} class="submit">Submit</button>
+    </div>;
+const Results = ({ state }) =>
+    <ul>
+        {state.questions.map((question, i) => answerCorrectness(state.correctness[i], question, state.answers[i]))}
+    </ul>;
+
 
 export default (state) =>
     <section>
         <h1>French number quiz</h1>
         {state.status === statuses.pending || state.status === statuses.ended
-            ? <button onclick={[actions.prepare, focusAnswerBox]} class="start">Start</button>
+            ? <StartButton />
             : null}
         {state.status === statuses.playing
-            ? <div>
-                <h2>Question {state.questionIndex + 1}</h2>
-                <input id="answerBox" type="text" onchange={actions.setAnswerText} value={state.answerText} maxlength="2" />
-                <button onclick={[actions.submit, focusAnswerBox]} class="submit">Submit</button>
-            </div>
+            ? <Question state={state} />
             : null}
         {state.status === statuses.ended
-            ? <ul>
-                {state.questions.map((question, i) => answerCorrectness(state.correctness[i], question, state.answers[i]))}
-            </ul>
+            ? <Results state={state} />
             : null}
     </section>;
